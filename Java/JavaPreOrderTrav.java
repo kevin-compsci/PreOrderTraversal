fc2edddb6e6bc05469d6f238d16013e53a3af67e
Kevin Nguyen <25444103+kevin-compsci@users.noreply.github.com>
2018-10-24 21:02:19 -0500

    Add files via upload

--- Java/JavaPreOrderTrav.java
+++ Java/JavaPreOrderTrav.java
@@ -5,7 +5,7 @@ Basic Java program that performs pre-order traversal iteratively.
 How to run (linux):
 	1.) open command line and cd into directory folder of where this code is
 	2.) type in "javac *.java" to compile java files
-	3.) type in "java JavaShell"
+	3.) type in "java JavaPreOrderTrav"
 */
 
 //imports
@@ -13,13 +13,18 @@ import java.io.*;
 import java.util.*;
 
 //class
-class JavaShell {
+class JavaPreOrderTrav {
 	//Global Declarations
 
 	//main driver
 	public static void main(String[] args) {
 		//local Declarations
-		System.out.println("HelloWorld");
+
+		//get tree
+
+		//call preorder
+
+		//output results
 	}
 
 	//call function to perform preorder traversal (iteration)
