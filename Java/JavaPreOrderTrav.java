0b0d4c6311e143ba571f6f7ad4e6c9e613423d18
Kevin Nguyen <25444103+kevin-compsci@users.noreply.github.com>
2018-10-24 20:47:50 -0500

    Add files via upload

---
+++ Java/JavaPreOrderTrav.java
@@ -0,0 +1,26 @@
+/*
+Kevin Nguyen
+Basic Shell for Java programs.
+
+How to run (linux):
+	1.) open command line and cd into directory folder of where this code is
+	2.) type in "javac *.java" to compile java files
+	3.) type in "java JavaShell"
+*/
+
+//imports
+import java.io.*;
+import java.util.*;
+
+//class
+class JavaShell {
+	//Global Declarations
+
+	//main driver
+	public static void main(String[] args) {
+		//local Declarations
+		System.out.println("HelloWorld");
+	}
+
+	//function definitions below	
+}
